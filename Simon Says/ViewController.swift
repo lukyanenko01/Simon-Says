c43b2ecd8bd9c106ae83d02e03fd4b2e73487579
Vladimir Lukyanenko <>
2023-02-28 21:36:33 +0200

    first commit

---
+++ Simon Says/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Vladimir Lukyanenko on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
